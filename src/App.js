23b61d7d4b1f7f28e2eb5e3c284e3897ca35a6cd
Cyril Gavala <cyril.gavala@gmail.com>
2021-11-13 18:51:34 +0100

    --- New version 1.1.0 --- - major redesign - small refactoring

--- src/App.js
+++ src/App.js
@@ -1,71 +1,15 @@
 import './App.css';
 import React, {useState} from "react";
-import {Button, Form, Modal, Spinner} from "react-bootstrap";
 import {GoogleLogin, GoogleLogout} from "react-google-login"
-import Papa from "papaparse";
-import Axios from 'axios'
+import EmailForm from "./components/EmailForm";
 
-const batchSize = parseInt(process.env.REACT_APP_BATCH_SIZE, 10)
-const timeout = parseInt(process.env.REACT_APP_TIMEOUT, 10)
+
 const client_id = process.env.REACT_APP_CLIENT_ID
 
 export default function App() {
 
-    const [headline, setHeadline] = useState("")
-    const [subject, setSubject] = useState("")
-    const [message, setMessage] = useState("")
-    const [addresses, setAddresses] = useState([])
-    const [isProcessing, setProcessing] = useState(false)
     const [user, setUser] = useState({})
     const [token, setToken] = useState({})
-
-    const clearState = () => {
-        setProcessing(false)
-        setHeadline("")
-        setSubject("")
-        setMessage("")
-        setAddresses([])
-    }
-
-    const handleSend = async () => {
-
-        const sendMail = async (addressesString) => {
-
-            const encodedMail = btoa("Content-Type: text/html; charset=\"UTF-8\"\nMIME-Version: 1.0\nContent-Transfer-Encoding: 7bit\n" +
-                `Subject: ${subject}\nFrom: ${headline}\nBcc: ${addressesString}\n\n${message}`
-            ).replace(/\+/g, '-').replace(/\//g, '_');
-            await Axios.post(`https://www.googleapis.com/gmail/v1/users/me/messages/send?access_token=${token.access_token}`,
-                {raw: encodedMail}).then((response) => {
-                console.log(response.data)
-            }).catch(err => {
-                console.error(err)
-                clearState()
-                alert("Oops, something went wrong. Check out console output")
-            })
-        }
-
-        setProcessing(true)
-        const batches = []
-        for (let i = 0, len = addresses.length; i < len; i += batchSize)
-            batches.push(addresses.slice(i, i + batchSize));
-        for (const value of batches) {
-            const index = batches.indexOf(value);
-            await sendMail(value.toString());
-            if (index < batches.length - 1)
-                await new Promise(r => setTimeout(r, timeout));
-        }
-        await clearState()
-        alert(`Process ended ${new Date().toLocaleString()}.`)
-    }
-
-    const handleFileSubmit = (event) => {
-        const fileFromEvent = event.target.files[0]
-        Papa.parse(fileFromEvent, {
-            complete: function (results) {
-                setAddresses(results.data.map(item => item[0]))
-            }
-        })
-    }
 
     const onSuccess = (res) => {
         setUser(res.profileObj)
@@ -86,16 +30,11 @@ export default function App() {
     const handleLogOut = () => {
         setUser({})
         setToken({})
-        clearState()
+        document.getElementById("email-form").reset();
     }
 
     return (
         <div className="App">
-            <Modal id={"spinner-modal"} show={isProcessing} backdrop="static" animation centered>
-                <Spinner id={"spinner"} animation="border" role="status" variant={"light"}>
-                    <span className="visually-hidden">Loading...</span>
-                </Spinner>
-            </Modal>
             <div id={"user-panel"}>
                 {Object.keys(user).length === 0 &&
                 <GoogleLogin id={"login-btn"} clientId={client_id} buttonText={"Log in"} isSignedIn={true}
@@ -106,30 +45,7 @@ export default function App() {
                               onLogoutSuccess={handleLogOut} theme={"dark"}/>}
             </div>
             <div id="main-container">
-                <div className="row">
-                    <Form.Control type="file" accept={".csv"} disabled={isProcessing}
-                                  onChange={event => handleFileSubmit(event)}/>
-                    <Button id="execute-btn" variant="outline-primary"
-                            disabled={addresses.length === 0 || isProcessing}
-                            onClick={handleSend}>
-                        {isProcessing ? "Sending..." : "Send"}
-                    </Button>
-                </div>
-                <div className="row">
-                    <Form.Control id="headline" as="input" size="md" type="text" placeholder="Headline"
-                                  disabled={isProcessing} value={headline}
-                                  onChange={event => setHeadline(event.target.value)}/>
-                </div>
-                <div className="row">
-                    <Form.Control id="subject" as="input" size="md" type="text" placeholder="Email subject"
-                                  disabled={isProcessing} value={subject}
-                                  onChange={event => setSubject(event.target.value)}/>
-                </div>
-                <div className="row">
-                    <Form.Control id="message" as="textarea" size="md" type="text" placeholder="Email message"
-                                  disabled={isProcessing} rows={10} value={message}
-                                  onChange={event => setMessage(event.target.value)}/>
-                </div>
+                <EmailForm token={token}/>
             </div>
         </div>
     );
---
+++ src/components/EmailForm.js
@@ -0,0 +1,92 @@
+import React, {useState} from "react";
+import Axios from "axios";
+import Papa from "papaparse";
+import {Modal, Spinner} from "react-bootstrap";
+
+export default function EmailForm(props) {
+
+    const batchSize = parseInt(process.env.REACT_APP_BATCH_SIZE, 10)
+    const timeout = parseInt(process.env.REACT_APP_TIMEOUT, 10)
+
+    const [isProcessing, setProcessing] = useState(false)
+    const [headline, setHeadline] = useState("")
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("")
+    const [addresses, setAddresses] = useState([])
+
+    const handleSend = async () => {
+
+        const sendMail = async (addressesString) => {
+
+            const encodedMail = btoa("Content-Type: text/html; charset=\"UTF-8\"\nMIME-Version: 1.0\nContent-Transfer-Encoding: 7bit\n" +
+                `Subject: ${subject}\nFrom: ${headline}\nBcc: ${addressesString}\n\n${message}`
+            ).replace(/\+/g, '-').replace(/\//g, '_');
+            await Axios.post(`https://www.googleapis.com/gmail/v1/users/me/messages/send?access_token=${props.token.access_token}`,
+                {raw: encodedMail}).then((response) => {
+                console.log(response.data)
+            }).catch(err => {
+                console.error(err)
+                clearState()
+                alert("Oops, something went wrong. Check out console output")
+            })
+        }
+
+        setProcessing(true)
+        const batches = []
+        for (let i = 0, len = addresses.length; i < len; i += batchSize)
+            batches.push(addresses.slice(i, i + batchSize));
+        for (const value of batches) {
+            const index = batches.indexOf(value);
+            await sendMail(value.toString());
+            if (index < batches.length - 1)
+                await new Promise(r => setTimeout(r, timeout));
+        }
+        await clearState()
+        alert(`Process ended ${new Date().toLocaleString()}.`)
+    }
+
+    const handleFileSubmit = (event) => {
+        const fileFromEvent = event.target.files[0]
+        Papa.parse(fileFromEvent, {
+            complete: function (results) {
+                setAddresses(results.data.map(item => item[0]))
+            }
+        })
+    }
+
+    const clearState = () => {
+        setProcessing(false)
+        setAddresses([])
+        document.getElementById("email-form").reset();
+    }
+
+    return (
+        <div id={"email-form-wrapper"}>
+            <Modal id={"spinner-modal"} show={isProcessing} backdrop="static" animation centered>
+                <Spinner id={"spinner"} animation="border" role="status" variant={"light"}>
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Modal>
+            <form id={"email-form"}>
+                <label htmlFor="file">Choose csv file containing email addresses</label>
+                <input id={"file"} type="file" accept={".csv"} disabled={isProcessing}
+                       onChange={event => handleFileSubmit(event)}
+                       placeholder={"Choose csv file containing email addresses"}/>
+                <input id="headline" type="text" placeholder="Headline" disabled={isProcessing} value={headline}
+                       onChange={event => setHeadline(event.target.value)}/>
+                <input id="subject" type="text" placeholder="Subject" disabled={isProcessing} value={subject}
+                       onChange={event => setSubject(event.target.value)}/>
+                <textarea id="message" rows="7" placeholder="Message" disabled={isProcessing} value={message}
+                          onChange={event => setMessage(event.target.value)}/>
+                <div id={"btn-wrapper"}>
+                    <button id="reset-btn" onClick={clearState}>Reset</button>
+                    <button id="execute-btn"
+                            disabled={props.token.access_token === undefined || addresses.length === 0 || isProcessing}
+                            onClick={handleSend}>
+                        {isProcessing ? "Sending..." : "Send"}
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
